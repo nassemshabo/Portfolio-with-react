cbed5e1a9201ab77351846471879d6b40df9bd92
nassemshabo <nassemshabo1@gmail.com>
2019-11-20 16:38:30 +0100

    first work

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import App from "./App";
+
+const Home = () => {
+  return <App />;
+};
+
+const Router = () => {
+  return (
+    <div>
+      <h1>Reouter</h1>
+    </div>
+  );
+};
+
+export default Router;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.header.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
